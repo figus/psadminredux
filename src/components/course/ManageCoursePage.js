bff7ed95c8da2dd45edae15d0f0b67cf4c65ff6e
figus <figus@users.noreply.github.com>
2018-05-21 20:59:24 +1000

    098 - Create Manage Course Page

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import * as courseActions from './../../actions/courseActions';
+
+class ManageCoursePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Manage Course</h1>
+            </div>
+        );
+    }
+}
+
+ManageCoursePage.propTypes = {
+    
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        state: state
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ManageCoursePage);
