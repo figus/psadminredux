457ea52fb8266d0a6e8a313a3304c896fb0f631a
figus <figus@users.noreply.github.com>
2018-05-21 13:03:21 +1000

    078 - Step Through Redux Flow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 
-class CoursesPage extends Component {
+class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
 
@@ -26,10 +26,15 @@ class CoursesPage extends Component {
         this.props.dispatch(courseActions.createCourse(this.state.course));
     }
 
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
+
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
                 <input type="submit" value="Save" onClick={this.onClickSave} />
@@ -37,6 +42,11 @@ class CoursesPage extends Component {
         );
     }
 }
+
+CoursesPage.propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    courses: React.PropTypes.array.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
     return {
