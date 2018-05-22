0ab8f6b6414b1cc09551c62b251b63876ea64dad
figus <figus@users.noreply.github.com>
2018-05-22 23:08:00 +1000

    136 - Testing Thunks

--- src/actions/courseActions.test.js
+++ src/actions/courseActions.test.js
@@ -2,12 +2,16 @@ import expect from 'expect';
 import * as courseActions from './courseActions';
 import * as types from './actionTypes';
 
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
 //Test a sync action
 describe('Course Actions', () => {
     describe('createCourseSuccess', () => {
         it('should create a CREATE_COURSE_SUCCESS action', () => {
             //arrange
-            const course = {id:'clean-code', title: 'Clean Code'};
+            const course = { id: 'clean-code', title: 'Clean Code' };
             const expectedAction = {
                 type: types.CREATE_COURSE_SUCCESS,
                 course: course
@@ -19,5 +23,44 @@ describe('Course Actions', () => {
             //assert
             expect(action).toEqual(expectedAction);
         });
+    });
+});
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    it('should create BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading courses', (done) => {
+        //Nock Example
+        // nock('http://www.example.com/')  //URL that it should hit (real one)
+        //     .get('/courses')
+        //     .reply(200, {                //Nock will capture the response and replace it with this
+        //         body: {
+        //             course [
+        //                 {id: 1, firstName: 'Cory', lastName: 'House'}
+        //             ]
+        //         }
+        //     });
+
+        //Arrange
+        const expectedActions = [
+            { type: types.BEGIN_AJAX_CALL },
+            { type: types.LOAD_COURSES_SUCCESS, body: { courses: [{ id: 'clean-code', title: 'Clean Code' }] } }
+        ];
+
+        //Act
+        const store = mockStore({ courses: [] }, expectedActions);
+        store.dispatch(courseActions.loadCourses())
+            .then(() => {
+                const actions = store.getActions();
+                expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+                expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
+                done();
+            });
+        //Assert
     });
 });
